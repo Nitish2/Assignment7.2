438687f39c22d3dd549cf9dbee710b320489bbb6
Nitish2 <nitishn886@gmail.com>
2017-06-12 15:28:00 +0530

    Assignment7.2

---
+++ src/TypePromotion.java
@@ -0,0 +1,13 @@
+
+public class TypePromotion {
+
+	public static void main(String[] args) {
+		char a= 'n';
+		int b= 5;
+		long sum= a + b ;     // will take ASCII code of small n for addition 
+		System.out.println("Sum is: "+sum);
+		
+
+	}
+
+}
